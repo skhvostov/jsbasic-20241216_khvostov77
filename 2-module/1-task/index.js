c862f5a6c7a8b553b1fd9a5f4376a9070c7fe4f5
skhvostov <khvostov77@yandex.ru>
2024-12-25 22:33:44 +0300

    second module tasks solutions

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
   // ваш код...
+  let result = 0;
+  if (Object.entries(salaries).length === 0) {
+    return result;
+  }
+  for (let key in salaries) {
+    if (Number.isFinite(salaries[key])) {
+      result = result + salaries[key];
+    }
+  }
+  return result;
 }

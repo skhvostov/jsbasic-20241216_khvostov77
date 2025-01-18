e777d9df72806138b2ad3027bf861ec47e727146
skhvostov <khvostov77@yandex.ru>
2025-01-18 18:04:37 +0300

    forth  module tasks solutions

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let friendsList = document.createElement('UL');
+
+  for (let friend of friends) {
+    friendsList.insertAdjacentHTML('beforeEnd', `<li>${friend.firstName} ${friend.lastName}</li>`)
+  }
+  
+  return friendsList;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
   // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    if (row.cells[2].textContent === 'm') {
+      row.classList.add('male');
+    } else {
+      row.classList.add('female');
+    }
+    if (Number(row.cells[1].textContent) < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+    if (row.cells[3].dataset.available === 'true') {
+      row.classList.add('available');
+    } else if (row.cells[3].dataset.available === 'false') {
+      row.classList.add('unavailable');
+    } else {
+      row.hidden = true;
+    }
+  }
 }

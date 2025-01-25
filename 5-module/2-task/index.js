6349d223e2e064055464a5d325c691aa396aa38d
skhvostov <khvostov77@yandex.ru>
2025-01-25 20:36:47 +0300

    fifth module tasks solutions

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
   // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let txt = document.querySelector('#text');
+
+  btn.addEventListener('click', () => {
+    if (txt.hidden) {
+      txt.hidden = false;
+    } else {
+      txt.hidden = true;
+    }
+  });
 }
